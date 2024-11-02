1dfc4b0f93b7d90ac4b444f73d02ef97da2912c5
Dmitry <79811924115@yandex.ru>
2024-11-02 10:45:55 +0300

    task3 ok

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+import Bowman from "./classes/Bowman";
+import Character from "./classes/Character";
+import Deamon from "./classes/Daemon";
+import Magician from "./classes/Magician";
+import Phonecontrol from "./classes/Phonecontrol";
+import Settings from "./classes/Settings";
+import Swordsman from "./classes/Swordsman";
+import Team from "./classes/Team";
+import Undead from "./classes/Undead";
+import Validator from "./classes/Validator";
+import Zombie from "./classes/Zombie";
